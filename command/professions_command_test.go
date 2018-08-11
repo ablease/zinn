2bbef246ba3e239365cc2c2e5b7011bbf6e4acef
Alex Blease <ablease@pivotal.io>
2018-08-11 09:45:30 +0100

    Professions returns an error when it fails
    Assert that when the command fails to get professions
    the command returns an error

--- command/professions_command_test.go
+++ command/professions_command_test.go
@@ -1,6 +1,8 @@
 package command_test
 
 import (
+	"errors"
+
 	"github.com/ablease/zinn/command"
 	"github.com/ablease/zinn/command/commandfakes"
 	"github.com/ablease/zinn/ui"
@@ -26,17 +28,46 @@ var _ = Describe("Professions Command", func() {
 		}
 	})
 
-	Describe("fetching professions", func() {
-		BeforeEach(func() {
-			fakeClient.ProfessionsReturns([]string{"prof1", "prof2"}, nil)
+	Describe("Setup", func() {
+		It("Sets the UI object", func() {
+			err = cmd.Setup(testUI)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(cmd.UI).To(Equal(testUI))
 		})
 
+		It("Sets the Client object", func() {
+			err = cmd.Setup(testUI)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(cmd.Client).ToNot(Equal(nil))
+		})
+	})
+
+	Describe("fetching professions", func() {
 		Context("when the command is successful", func() {
+			BeforeEach(func() {
+				fakeClient.ProfessionsReturns([]string{"prof1", "prof2"}, nil)
+			})
+
 			It("displays the list of professions", func() {
 				err = cmd.Execute(nil)
 				Expect(err).ToNot(HaveOccurred())
 				Expect(fakeClient.ProfessionsCallCount()).To(Equal(1))
 				Expect(testUI.Out).To(gbytes.Say("prof1 prof2"))
+			})
+		})
+
+		Context("when the command is unsuccessful", func() {
+			Context("due to a client error", func() {
+				BeforeEach(func() {
+					fakeClient.ProfessionsReturns(nil, errors.New("oops"))
+				})
+
+				It("displays the list of professions", func() {
+					err = cmd.Execute(nil)
+					Expect(err).To(HaveOccurred())
+					Expect(fakeClient.ProfessionsCallCount()).To(Equal(1))
+					Expect(err.Error()).To(Equal("oops"))
+				})
 			})
 		})
 	})
